8ba55f7ba4d461e522c0653903dd974feb5f3d1b
dsh91698 <mettus@mail.ru>
2023-06-01 23:32:58 +0400

    feat: all cars show on in cars.html

--- src/main/java/web/controller/CarsController.java
+++ src/main/java/web/controller/CarsController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import model.Car;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,15 +13,20 @@ import java.util.List;
 
 @Controller
 public class CarsController {
+    static List<Car> cars = new ArrayList<>();
+
+    static {
+        cars.add(new Car("Blackbird", "Kia", 2023));
+        cars.add(new Car("Kolibri", "Nissan", 2022));
+        cars.add(new Car("Bullrider", "BMW", 2021));
+        cars.add(new Car("Pontecorvo", "Cadillac", 2020));
+        cars.add(new Car("Chupacabra", "Jeep", 2019));
+    }
 
     @GetMapping(value = "/cars")
-    public String printWelcome(ModelMap model) {
-        List<String> messages = new ArrayList<>();
-        messages.add("Cars is good!!!");
-        messages.add("I'm Spring MVC application mozerfucker");
-        messages.add("5.2.0 version by sep'19 ");
-        model.addAttribute("messages", messages);
-        return "index";
+    public String printCars(ModelMap model) {
+        model.addAttribute("cars", cars);
+        return "cars";
     }
 
 }
